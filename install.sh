557a0d786fd587fcee1753a34c2c4885c415b5b1
viggle-by <barlevsj@gmail.com>
2025-10-14 17:11:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-pacman -Sy --needed git base-devel
-cd /opt
-sudo git clone https://aur.archlinux.org/yay-git.git
+pacman -Sy --needed git base-devel && cd /opt && git clone https://aur.archlinux.org/yay-git.git
