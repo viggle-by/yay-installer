4690415c4d502a656b23632783a45b8671869332
viggle-by <barlevsj@gmail.com>
2025-10-15 20:23:09 -0400

    ts pmo

--- arch.sh
+++ arch.sh
@@ -1,3 +1,4 @@
+## setup cmds
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
